9d6d63755a3512a8070d26fb27f3c56a7849320f
alpertSnow <rmddxx@163.com>
2017-10-22 14:44:01 +0800

    Smaller sampling number. 30k in total.

--- 03runJags.R
+++ 03runJags.R
@@ -15,7 +15,7 @@ parameters <- c('i','j','q','x','y')
 inits <- NULL
 #inits <- function() {list(icon = 26, jcon = 31, q = 60)}
 
-sf.sim <- jags(data, inits, parameters, "sf.bug",n.chains=3,n.iter=50000,n.burnin=5000,n.thin=1)
+sf.sim <- jags(data, inits, parameters, "sf.bug",n.chains=3,n.iter=10000,n.burnin=1000,n.thin=1)
 
 
 sf.coda <- as.mcmc(sf.sim)
